59476f596aeaee194736c126b37398186e996bac
Vasyl Diakiv <vasyldyakiv@gmail.com>
2019-01-13 21:07:43 +0200

    greeting text added

--- screens/GreetingScreen.js
+++ screens/GreetingScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default class GreetingScreen extends React.Component {
   static navigationOptions = {
@@ -7,7 +7,15 @@ export default class GreetingScreen extends React.Component {
   };
 
   render() {
-    return <ScrollView style={styles.container} />;
+    return (
+      <ScrollView style={styles.container}>
+        <View style={styles.textContainer}>
+          <Text style={styles.greetingText}>
+            Hello in my first React Native app
+          </Text>
+        </View>
+      </ScrollView>
+    );
   }
 }
 
@@ -16,5 +24,15 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 15,
     backgroundColor: "#fff"
+  },
+  textContainer: {
+    backgroundColor: "rgb(0,150,0)",
+    alignItems: "center",
+    marginHorizontal: 50
+  },
+  greetingText: {
+    fontSize: 25,
+    lineHeight: 24,
+    textAlign: "center"
   }
 });
