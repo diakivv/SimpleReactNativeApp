c95b5b1ef79c05ca6bf2580ce1941d8f370e1f5a
Vasyl Diakiv <vasyldyakiv@gmail.com>
2019-01-13 21:28:59 +0200

    image & about me text added

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, View, Image, Text } from "react-native";
 
 export default class AboutScreen extends React.Component {
   static navigationOptions = {
@@ -7,7 +7,22 @@ export default class AboutScreen extends React.Component {
   };
 
   render() {
-    return <ScrollView style={styles.container} />;
+    return (
+      <ScrollView style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../assets/images/me.jpg")}
+            style={styles.meImage}
+          />
+        </View>
+
+        <View style={styles.aboutMeContainer}>
+          <Text style={styles.aboutMeText}>
+            I'm Vasyl, almost 21-years-old programmer
+          </Text>
+        </View>
+      </ScrollView>
+    );
   }
 }
 
@@ -16,5 +31,27 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 15,
     backgroundColor: "#fff"
+  },
+  imageContainer: {
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 20
+  },
+  meImage: {
+    width: 400,
+    height: 320,
+    resizeMode: "contain",
+    marginTop: 3,
+    marginLeft: -10
+  },
+  aboutMeContainer: {
+    alignItems: "center",
+    marginHorizontal: 50
+  },
+  aboutMeText: {
+    fontSize: 17,
+    color: "rgba(96,100,109, 1)",
+    lineHeight: 24,
+    textAlign: "center"
   }
 });
